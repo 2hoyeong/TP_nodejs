0faa7251b6cf4c98131f56c32ff251890d709071
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2020-06-12 20:22:26 +0900

    travis ci test

---
+++ test/index.spec.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const request = require('supertest');
+const should = require('should');
+
+const app = require('../index');
+
+describe('GET /', () => {
+  it('index should contain Hello', (done) => {
+    request(app)
+        .get('/')
+        .expect(200)
+        .expect((res) => {
+            res.text.should.containEql('Hello World!');
+        })
+        .end((err, res) => {
+            if (err) throw err;
+            done();
+        });
+  });
+});
