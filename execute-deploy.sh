375159a80d5d75dc78415c641915c31d19ca55e8
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2020-06-13 19:09:22 +0900

    Update execute-deploy.sh

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # rm /home/ec2-user/docker-image/tp/TP_nodejs/beforeInstall.sh
-/home/ec2-user/docker-image/deploy.sh
+. /home/ec2-user/docker-image/deploy.sh
 sleep 5
 echo "deploy finished"
