576c8ffcba48a29a7b04fe0c34cbdb4657b7ccdf
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2020-06-13 17:57:33 +0900

    Update beforeInstall.bash

--- scripts/beforeInstall.bash
+++ scripts/beforeInstall.bash
@@ -3,3 +3,4 @@ if [ -d /home/ec2-user/docker-image/tp/TP_nodejs ]; then
         sudo rm -rf /home/ec2-user/docker-image/tp/TP_nodejs
 fi
 sudo mkdir -vp /home/ec2-user/docker-image/tp/TP_nodejs
+echo "directory clear"
