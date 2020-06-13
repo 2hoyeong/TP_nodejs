c02e3dae3f1ed0ab1ac217eb0782e9e79721c22b
2hoyeong <leehodud302@naver.com>
2020-06-13 13:37:22 +0900

    ci/cd test

--- scripts/execute-deploy.sh
+++ scripts/execute-deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-rsync --delete-before --verbose --archive --exclude " . * " --exclude "node_modules/*" > /var/log/deploy.log
-
-cd /home/ec2-user/docker-image
-./deploy.sh > /dev/null 2> /dev/null < /dev/null &
+# rsync --delete-before --verbose --archive --exclude " . * " --exclude "node_modules/*" > /var/log/deploy.log
+/home/ec2-user/docker-image/deploy.sh > /dev/null 2> /dev/null < /dev/null &
