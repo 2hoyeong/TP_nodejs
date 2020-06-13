f71d1f6c3e9e4a7ec2f4872fffdfa52b70cf5a10
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2020-06-13 16:39:21 +0900

    ci/cd test

--- scripts/execute-deploy.sh
+++ scripts/execute-deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-rsync --delete-before --verbose --archive --exclude " . * " --exclude "node_modules/*" > /var/log/deploy.log
+#rsync --delete-before --verbose --archive --exclude " . * " --exclude "node_modules/*" > /var/log/deploy.log
 /home/ec2-user/docker-image/deploy.sh > /dev/null 2> /dev/null < /dev/null &
