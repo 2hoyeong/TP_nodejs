b0b4f0e96377c466fa1b2b59aeee182d35f73dbb
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2020-06-13 18:45:01 +0900

    ci/cd last test

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo rm /home/ec2-user/docker-image/tp/TP_nodejs/beforeInstall.sh
+rm /home/ec2-user/docker-image/tp/TP_nodejs/beforeInstall.sh
 cd /home/ec2-user/docker-image
-sudo ./deploy.sh
+./deploy.sh
 sleep 5
 echo "deploy finished"
