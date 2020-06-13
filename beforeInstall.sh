7b3426189ea08a10fab6c11a13c6fd0de68a53ee
2hoyeong <leehodud302@naver.com>
2020-06-13 12:19:01 +0900

    ci/cd test

---
+++ beforeInstall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ -d /home/ec2-user/docker-image/tp/TP_nodejs ]; then
+        sudo rm -rf /home/ec2-user/docker-image/tp/TP_nodejs
+fi
+sudo mkdir -vp /home/ec2-user/docker-image/tp/TP_nodejs
